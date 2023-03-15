78ecd8587b68ae7ec946ac36c677d49adcb6e247
Jun Aruga <jaruga@redhat.com>
2023-03-15 20:30:06 +0100

    Fixed the issue to enable FIPS mode.
    It seems that it works now.

--- fips_mode.c
+++ fips_mode.c
@@ -4,13 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    OSSL_LIB_CTX *libctx = NULL;
     int status = EXIT_SUCCESS;
     int fips_enabled = 0;
 
-    libctx = OSSL_LIB_CTX_new();
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
-    OSSL_LIB_CTX_free(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
+err:
     printf("FIPS mode: %d\n", fips_enabled);
     exit(status);
 }
