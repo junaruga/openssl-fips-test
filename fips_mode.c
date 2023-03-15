2cec8a797f531532ca684772432319d5d86c958a
Jun Aruga <jaruga@redhat.com>
2023-03-15 20:12:27 +0100

    Add the free function.

--- fips_mode.c
+++ fips_mode.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 
     libctx = OSSL_LIB_CTX_new();
     fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    OSSL_LIB_CTX_free(libctx);
     printf("FIPS mode: %d\n", fips_enabled);
     exit(status);
 }
