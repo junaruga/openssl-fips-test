a1a8f8b28e4ef2e79a45166b05ee9c8e276ae7e8
Jun Aruga <jaruga@redhat.com>
2023-03-16 18:01:13 +0100

    Remove unused label.

--- fips_mode.c
+++ fips_mode.c
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     int fips_enabled = 0;
 
     fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
-err:
     printf("FIPS mode: %d\n", fips_enabled);
     exit(status);
 }
