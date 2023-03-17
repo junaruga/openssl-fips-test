04a02ac32c65fcac8ac27b8b7bae63cb34bb8e1d
Jun Aruga <jaruga@redhat.com>
2023-03-17 22:18:33 +0100

    Implement the fips_mode_set without OSSL_LIB_CTX *libctx variable.
    I was able to do it.

--- fips_mode_set.c
+++ fips_mode_set.c
@@ -5,50 +5,45 @@ int main(int argc, char *argv[])
 {
     int status = EXIT_SUCCESS;
     int fips_enabled = 0;
-    OSSL_LIB_CTX *libctx = NULL;
 
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode: %d\n", fips_enabled);
 
-    libctx = OSSL_LIB_CTX_new();
     printf("FIPS mode on.\n");
-    if (!EVP_default_properties_enable_fips(libctx, 1)) {
+    if (!EVP_default_properties_enable_fips(NULL, 1)) {
         status = EXIT_FAILURE;
         goto err;
     }
 
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode: %d\n", fips_enabled);
 
     printf("FIPS mode on 2nd time.\n");
-    if (!EVP_default_properties_enable_fips(libctx, 1)) {
+    if (!EVP_default_properties_enable_fips(NULL, 1)) {
         status = EXIT_FAILURE;
         goto err;
     }
 
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode: %d\n", fips_enabled);
 
     printf("FIPS mode off.\n");
-    if (!EVP_default_properties_enable_fips(libctx, 0)) {
+    if (!EVP_default_properties_enable_fips(NULL, 0)) {
         status = EXIT_FAILURE;
         goto err;
     }
 
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode: %d\n", fips_enabled);
 
     printf("FIPS mode off 2nd time.\n");
-    if (!EVP_default_properties_enable_fips(libctx, 0)) {
+    if (!EVP_default_properties_enable_fips(NULL, 0)) {
         status = EXIT_FAILURE;
         goto err;
     }
 
-    fips_enabled = EVP_default_properties_is_fips_enabled(libctx);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode: %d\n", fips_enabled);
 err:
-    if (libctx) {
-        OSSL_LIB_CTX_free(libctx);
-    }
     exit(status);
 }
