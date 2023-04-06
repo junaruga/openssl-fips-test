8a3e508f679a0b92186dc9ef8c7f17f0a925423d
Jun Aruga <jaruga@redhat.com>
2023-04-06 17:09:16 +0200

    Add the base provider printing logic.

--- fips_mode.c
+++ fips_mode.c
@@ -5,11 +5,15 @@
 int main(int argc, char *argv[])
 {
     int status = EXIT_SUCCESS;
+    int base_available = 0;
     int fips_available = 0;
     int fips_enabled = 0;
 
+    base_available = OSSL_PROVIDER_available(NULL, "base");
+    printf("Base provider available: %d\n", base_available);
+
     fips_available = OSSL_PROVIDER_available(NULL, "fips");
-    printf("FIPS mode provider available: %d\n", fips_available);
+    printf("FIPS provider available: %d\n", fips_available);
 
     fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
     printf("FIPS mode enabled: %d\n", fips_enabled);
