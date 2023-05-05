b177d01e5ba7c66c296ed1235f6d7de5d90c1031
Jun Aruga <jaruga@redhat.com>
2023-05-05 16:19:14 +0200

    Add OpenSSL trace API test.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = gcc
 SRCS_GET = fips_mode.c
 SRCS_SET = fips_mode_set.c
+SRCS_TRACE = trace.c
 CFLAGS = $(OPTFLAGS) $(DEBUGFLAGS)
 OPTFLAGS = -O0
 DEBUGFLAGS = -g3 -ggdb3 -gdwarf-5
@@ -8,21 +9,27 @@ INCFLAGS =
 LDFLAGS =
 OBJS_GET = $(SRCS_GET:.c=.o)
 OBJS_SET = $(SRCS_SET:.c=.o)
+OBJS_TRACE = $(SRCS_TRACE:.c=.o)
+
 EXE = fips_mode
 EXE_SET = fips_mode_set
-EXE_ALL= $(EXE) $(EXE_SET)
+EXE_TRACE = trace
+EXE_ALL= $(EXE) $(EXE_SET) $(EXE_TRACE)
 LIBS = -lssl -lcrypto
 
 .c.o :
 	$(CC) -c $(CFLAGS) $(INCFLAGS) $< -o $@
 
 .PHONY: all
-all : $(EXE) $(EXE_SET)
+all : $(EXE) $(EXE_SET) $(EXE_TRACE)
 
 $(EXE) : $(OBJS_GET)
 	$(CC) $(LDFLAGS) $^ -o $@ $(LIBS)
 
 $(EXE_SET) : $(OBJS_SET)
+	$(CC) $(LDFLAGS) $^ -o $@ $(LIBS)
+
+$(EXE_TRACE) : $(OBJS_TRACE)
 	$(CC) $(LDFLAGS) $^ -o $@ $(LIBS)
 
 .PHONY: clean
@@ -39,6 +46,11 @@ test :
 test-set :
 	@echo "Testing..."
 	./$(EXE_SET)
+
+.PHONY: test-trace
+test-trace :
+	@echo "Testing..."
+	./$(EXE_TRACE)
 
 .PHONY: test-strace
 strace-test :
---
+++ trace.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <openssl/bio.h>
+#include <openssl/trace.h>
+
+int main(int argc, char *argv[])
+{
+    int status = EXIT_SUCCESS;
+    BIO *trace_trace_bio = NULL;
+    BIO *trace_decoder_bio = NULL;
+    int trace_trace_enabled = 0;
+    int trace_decoder_enabled = 0;
+
+    /*
+     * Traces the OpenSSL trace API itself by setting the category
+     * OSSL_TRACE_CATEGORY_TRACE. See `man 3 OSSL_trace_set_channel`.
+     */
+    trace_trace_bio = BIO_new_fp(stderr, BIO_NOCLOSE | BIO_FP_TEXT);
+    OSSL_trace_set_channel(OSSL_TRACE_CATEGORY_TRACE, trace_trace_bio);
+    OSSL_trace_set_prefix(OSSL_TRACE_CATEGORY_TRACE, "BEGIN TRACE TRACE");
+    OSSL_trace_set_suffix(OSSL_TRACE_CATEGORY_TRACE, "END TRACE TRACE");
+
+    trace_trace_enabled = OSSL_trace_enabled(OSSL_TRACE_CATEGORY_TRACE);
+    printf("TRACE - Trace enabled: %d\n", (trace_trace_enabled) ? 1 : 0);
+
+    /*
+     * Traces decoder operations by setting the category
+     * OSSL_TRACE_CATEGORY_DECODER.
+     */
+    trace_decoder_bio = BIO_new_fp(stderr, BIO_NOCLOSE | BIO_FP_TEXT);
+    OSSL_trace_set_channel(OSSL_TRACE_CATEGORY_DECODER, trace_decoder_bio);
+    OSSL_trace_set_prefix(OSSL_TRACE_CATEGORY_DECODER, "BEGIN DECODER TRACE");
+    OSSL_trace_set_suffix(OSSL_TRACE_CATEGORY_DECODER, "END DECODER TRACE");
+
+    trace_decoder_enabled = OSSL_trace_enabled(OSSL_TRACE_CATEGORY_DECODER);
+    printf("DECODER - Trace enabled: %d\n", (trace_decoder_enabled) ? 1 : 0);
+
+    exit(status);
+}
